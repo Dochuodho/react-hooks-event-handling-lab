e0f3dbd3c52181d793cdf65e765e3abd3a51b918
Ochuodho <ochuodhoderrick@gmail.com>
2023-08-20 09:19:36 +0300

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function handleChange(event){
+        console.log(event.target.value);
+
+
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChange} placeholder = "Entering password ..." />
+
+
+        </div>
     )
 }
 
